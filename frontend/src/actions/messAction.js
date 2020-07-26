19d17f0555bc666ae304596553a05ef38ac886f0
fred62879 <zhuotingxie-fred@outlook.com>
2020-07-26 11:13:05 -0700

    ass6

--- frontend/src/actions/messAction.js
+++ frontend/src/actions/messAction.js
@@ -16,19 +16,19 @@ export const uload = txt => {
 
 export const update = newmess => {
     return {
-	type: at.MESS_UPDATE,
-	newmess: newmess
+        type: at.MESS_UPDATE,
+        newmess: newmess
     };
 }
 
 export const store = (tp, messes, item, id, txt, un) => {
     return {
         type: tp, // 'INI_RECEIVE', 'ITEM_RECEIVE', 'ITEM_LOADING', 'MESS_ADD', 'MESS_CLEAR'
-	messes: messes,
-	messitem: item,
-	id: id,
+        messes: messes,
+        messitem: item,
+        id: id,
         newmess: txt,
-	username: un
+        username: un
     };
 }
 
@@ -41,51 +41,51 @@ export const display = cho => {
 
 export const redirect = (type, url) => {
     return {
-	type: type,
-	link: url
+        type: type,
+        link: url
     }
 }
 
 export const login = username => {
     return {
-	type: at.LOGIN,
-	username: username
+        type: at.LOGIN,
+        username: username
     };
 }
 
 export const detail = messdetail => {
     return {
-	type: at.DETAIL_VIEW,
-	content: messdetail
+        type: at.DETAIL_VIEW,
+        content: messdetail
     }
 }
 
 export function fetchMessages() {
     return dispatch => {
-	return fetch('http://localhost:3001/messages')
-	    .then((response) => {
-		return response.json();
-	    }).then((res) => {
-		dispatch(store(at.INI_RECEIVE, res.data, {}, "", "", ""));
-		return res.data;
-	    }).catch((err) => {
-		console.log(err);
-	    });
+        return fetch('/messages')
+            .then((response) => {
+                return response.json();
+            }).then((res) => {
+                dispatch(store(at.INI_RECEIVE, res.data, {}, "", "", ""));
+                return res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
     }
 }
 
 export function fetchMessItem(id) {
     return dispatch => {
-        return fetch(`http://localhost:3001/messages/detail/${id}`)
-	    .then((response) => {
-		return response.json();
-	    }).then((res) => {
-		console.log("fetch");
-		console.log(res);
-		dispatch(store(at.ITEM_RECEIVE, [], res.data, "", "", ""));
-	    }).catch((e) => {
-		console.log(e)
-	    });
+        return fetch(`/messages/detail/${id}`)
+            .then((response) => {
+                return response.json();
+            }).then((res) => {
+                console.log("fetch");
+                console.log(res);
+                dispatch(store(at.ITEM_RECEIVE, [], res.data, "", "", ""));
+            }).catch((e) => {
+                console.log("ERROR" + e);
+            });
     }
 }
 
@@ -97,48 +97,48 @@ export function messItemLoading() {
 
 export function postMessItem(mess, un) {
     return dispatch => {
-	return fetch(`http://localhost:3001/messages`, {
-	    method: 'POST',
-	    headers: {
-    		'Accept': 'application/json',
-    		'Content-Type': 'application/json',
-	    },
-	    body: JSON.stringify({
-		id: "",
-		mess: mess,
-		username: un
-	    })
-	}).then((response) => {
-	    if (!response.ok) {
-		throw Error(response.statusText);
-	    } else {
-		return response.json();
-	    }
-	}).then((res) => {
-	    dispatch(store(at.MESS_ADD, [], {}, res.data.id, res.data.mess, res.data.username));
-	}).catch( (e) => console.log(e) );
+        return fetch(`/messages`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: "",
+                mess: mess,
+                username: un
+            })
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        }).then((res) => {
+            dispatch(store(at.MESS_ADD, [], {}, res.data.id, res.data.mess, res.data.username));
+        }).catch((e) => console.log(e));
     }
 }
 
 export function updateMessItem(newmess, id) {
     return dispatch => {
-	return fetch(`http://localhost:3001/messages/update/${id}`, {
-	    method: 'PUT',
-	    headers: {
-		    'Accept': 'application/json',
-    		'Content-Type': 'application/json',
-	    },
-	    body: JSON.stringify({
-		mess: newmess
-	    })
-	}).then((response) => {
-	    if (!response.ok) {
-		throw Error(response.statusText);
-	    } else {
-		return response.json();
-	    }
-	}).then((res) => {
-	    fetchMessages();
-	}).catch( (e) => console.log(e) );
+        return fetch(`/messages/update/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                mess: newmess
+            })
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        }).then((res) => {
+            fetchMessages();
+        }).catch((e) => console.log(e));
     }
 }
